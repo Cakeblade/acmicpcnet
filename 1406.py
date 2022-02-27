287da0e49714edae362f5d174cfa5a0928c35f2e
Cakeblade <jch1070@gmail.com>
2022-02-28 01:27:53 +0900

    02/28

--- 1406.py
+++ 1406.py
@@ -1,6 +1,25 @@
 import sys
 
-s = sys.stdin.readline()
-index = len(s) - 1
+s1 = list(sys.stdin.readline())
+s2 = []
+del s1[-1]
+n = int(sys.stdin.readline())
 
-print(index)
+for i in range(n):
+    cmd = list(map(str, sys.stdin.readline().split()))
+    if cmd[0] == 'L' and s1 != []:
+        s2.append(s1.pop())
+
+    elif cmd[0] == 'D' and s2 != []:
+        s1.append(s2.pop())
+
+    elif cmd[0] == "B" and s1 != []:
+        s1.pop()
+
+    elif cmd[0] == "P":
+        s1.append(cmd[1])
+
+    else:
+        continue
+    
+print("".join(s1 + list(reversed(s2))))
