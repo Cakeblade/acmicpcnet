3119b530f7b45a3b41a8372477161721f0ed183c
Cakeblade <jch1070@gmail.com>
2023-03-08 11:24:08 +0900

    rename solved folder

--- python/11051.py
+++
@@ -1,6 +0,0 @@
-import sys
-from math import factorial
-
-n, k = map(int, input().split())
-output = factorial(n) // (factorial(k) * factorial(n - k))
-print(output % 10007)
--- python/8393.py
+++
@@ -1,5 +0,0 @@
-sum = 0
-n = int(input())
-for i in range(n+1):
-	sum = sum + i
-print(sum)
